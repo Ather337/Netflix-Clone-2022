4c94856ed6d28fda7b791a91e09786f6a8ada16a
Ather337 <mathar337@gmail.com>
2022-01-27 00:21:25 +0500

    =My Second Commit

---
+++ src/pages/Browse.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function browse() {
+  return 
+  <div>
+    <Hearder />
+    <BrowesWrapper>
+    <Row />
+    <Row />
+    <Row />
+    </BrowesWrapper>
+    <Footer />
+  </div>;
+}
